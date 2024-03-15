6853ae49a831d35b3c3bfc936f4cc4dd875c8474
Ricardo Campos <rickoliver001@hotmail.com>
2024-03-15 12:21:10 -0300

    :sparkles: feat: Initial Commit

---
+++ rest-with-springboot/src/main/java/com/nstech/restwithspringboot/RestWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nstech.restwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ rest-with-springboot/src/test/java/com/nstech/restwithspringboot/RestWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nstech.restwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
